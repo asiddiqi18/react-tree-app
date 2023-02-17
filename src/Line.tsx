3a1d1211be6d3ac0c159406c7cf9baf23b2b75e3
asiddiqi18 <marmar18@live.com>
2023-02-17 15:03:06 -0800

    extract Line component outside

---
+++ src/Line.tsx
@@ -0,0 +1,58 @@
+import { CSSProperties } from "react";
+
+type Props = {
+  fromRect: DOMRect | null;
+  toRect: DOMRect | null;
+};
+
+const LineTo: React.FC<Props> = ({ fromRect, toRect }) => {
+  if (!fromRect || !toRect) {
+    return null;
+  }
+
+  const left = Math.min(fromRect.left, toRect.left);
+  const right = Math.max(fromRect.right, toRect.right);
+  const top = Math.min(fromRect.top, toRect.top);
+  const bottom = Math.max(fromRect.bottom, toRect.bottom);
+  const width = right - left;
+  const height = bottom - top;
+  const x1 = fromRect.left + fromRect.width / 2 - left;
+  const y1 = fromRect.top + fromRect.height / 2 - top;
+  const x2 = toRect.left + toRect.width / 2 - left;
+  const y2 = toRect.top + toRect.height / 2 - top;
+
+  const style: CSSProperties = {
+    position: "absolute",
+    left: left,
+    top: top,
+    width: width,
+    height: height,
+    pointerEvents: "none",
+    zIndex: -1,
+  };
+  const svgStyle: CSSProperties = {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    width: width,
+    height: height,
+    pointerEvents: "none",
+  };
+  const pathStyle: CSSProperties = {
+    fill: "none",
+    stroke: "black",
+    strokeWidth: "2px",
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+  };
+
+  return (
+    <div style={style}>
+      <svg style={svgStyle}>
+        <path d={`M ${x1} ${y1} L ${x2} ${y2}`} style={pathStyle} />
+      </svg>
+    </div>
+  );
+};
+
+export default LineTo;
--- src/TreeGraph.tsx
+++ src/TreeGraph.tsx
@@ -1,6 +1,6 @@
-import { CSSProperties, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TreeNode } from "./tree";
-import debounce from "lodash/debounce";
+import Line from "./Line";
 
 function NodeDisplay({ node, parentRef }: { node: TreeNode; parentRef: any }) {
   const nodeRef = useRef<HTMLDivElement>(null);
@@ -34,7 +34,7 @@ function NodeDisplay({ node, parentRef }: { node: TreeNode; parentRef: any }) {
       >
         {node.value}
       </div>
-      <LineTo fromRect={fromRect} toRect={toRect} />
+      <Line fromRect={fromRect} toRect={toRect} />
       {node.children && node.children.length > 0 && (
         <div className="flex">
           {node.children.map((child) => (
@@ -57,130 +57,9 @@ function TreeGraph({ root }: { root: TreeNode }) {
   }
 }
 
-type Props = {
-  fromRect: DOMRect | null;
-  toRect: DOMRect | null;
-};
 
 
-const Line: React.FC<Props> = ({ fromRect, toRect }) => {
-  if (!fromRect || !toRect) {
-    return null;
-  }
-
-  const left = Math.min(fromRect.left, toRect.left);
-  const right = Math.max(fromRect.right, toRect.right);
-  const top = Math.min(fromRect.top, toRect.top);
-  const bottom = Math.max(fromRect.bottom, toRect.bottom);
-  const width = right - left;
-  const height = bottom - top;
-  const x1 = fromRect.left + fromRect.width / 2 - left;
-  const y1 = fromRect.top + fromRect.height / 2 - top;
-  const x2 = toRect.left + toRect.width / 2 - left;
-  const y2 = toRect.top + toRect.height / 2 - top;
-
-  const style: CSSProperties = {
-    position: "absolute",
-    left: left,
-    top: top,
-    width: width,
-    height: height,
-    pointerEvents: "none",
-  };
-  const svgStyle: CSSProperties = {
-    position: "absolute",
-    left: 0,
-    top: 0,
-    width: width,
-    height: height,
-    pointerEvents: "none",
-  };
-  const pathStyle: CSSProperties = {
-    fill: "none",
-    stroke: "black",
-    strokeWidth: "2px",
-    strokeLinecap: "round",
-    strokeLinejoin: "round",
-  };
-
-  return (
-    <div style={style}>
-      <svg style={svgStyle}>
-        <path d={`M ${x1} ${y1} L ${x2} ${y2}`} style={pathStyle} />
-      </svg>
-    </div>
-  );
-};
 
 
-function LineTo({
-  fromRect,
-  toRect,
-}: {
-  fromRect: DOMRect | null;
-  toRect: DOMRect | null;
-}) {
-  console.log("LineTo")
-  if (fromRect && toRect) {
-    const left = Math.min(fromRect.left, toRect.left);
-    const right = Math.max(fromRect.right, toRect.right);
-    const top = Math.min(fromRect.top, toRect.top);
-    const bottom = Math.max(fromRect.bottom, toRect.bottom);
-    const width = right - left;
-    const height = bottom - top;
-    const x1 = fromRect.left + fromRect.width / 2 - left;
-    const y1 = fromRect.top + fromRect.height / 2 - top;
-    const x2 = toRect.left + toRect.width / 2 - left;
-    const y2 = toRect.top + toRect.height / 2 - top;
-
-    // console.log({ from: [x1, y1], to: [x2, y2] });
-
-    const style: CSSProperties = {
-      position: "absolute",
-      left: left,
-      top: top,
-      width: width,
-      height: height,
-      pointerEvents: "none",
-    };
-    const svgStyle: CSSProperties = {
-      position: "absolute",
-      left: 0,
-      top: 0,
-      width: width,
-      height: height,
-      pointerEvents: "none",
-    };
-    const pathStyle: CSSProperties = {
-      fill: "none",
-      stroke: "black",
-      strokeWidth: "2px",
-      strokeLinecap: "round",
-      strokeLinejoin: "round",
-    };
-
-    return (
-      // <div style={style}>
-      //   <svg style={svgStyle}>
-      //     <path d={`M ${x1} ${y1} L ${x2} ${y2}`} style={pathStyle} />
-      //   </svg>
-      // </div>
-      // <div className="line ">
-      //   <div
-      //     id="from-parent"
-      //     className={`absolute top-[${600}px] left-[${600}px] p-1 opacity-25 bg-red-500`}
-      //   ></div>
-      //   <div
-      //     id="to-child"
-      //     className={`absolute top-[${600}px] left-[${600}px] p-1 opacity-25 bg-blue-500`}
-      //   ></div>
-      // </div>
-      <Line fromRect={fromRect} toRect={toRect} />
-    );
-  } else {
-    console.log("null", {fromRect, toRect});
-    return null;
-  }
-}
 
 export default TreeGraph;
